e9c5092456738598f40772665cc2a98d0a52c5b0
briguetjo <briguetjo@gmail.com>
2023-02-05 22:36:40 +0100

    fix download

--- download.py
+++ download.py
@@ -7,7 +7,7 @@ from transformers import T5ForConditionalGeneration, T5Tokenizer
 
 def download_model():
     model_name = "mrm8488/flan-t5-large-finetuned-openai-summarize_from_feedback"
-    model = T5ForConditionalGeneration.from_pretrained(model_name).to("cuda")
+    model = T5ForConditionalGeneration.from_pretrained(model_name)
     tokenizer = T5Tokenizer.from_pretrained(model_name)
 
 if __name__ == "__main__":
